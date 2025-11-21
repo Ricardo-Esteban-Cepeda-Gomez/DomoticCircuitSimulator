1049b5dd90a6ddd65eadfb86ce4f0dd82789dea1
Ricardo-Esteban-Cepeda-Gomez <rcepeda@unal.edu.co>
2025-11-21 12:36:53 -0500

    Update alarm.py

--- src/classes/snippets/alarm.py
+++ src/classes/snippets/alarm.py
@@ -1,7 +1,7 @@
 from Component import component
 class alarm(component):
     def __init__ (self):
-        isOn: False
-        volume: 0
+        isOn = False
+        volume = 0
     def toggle(self):
         self.isOn = not self.isOn 
